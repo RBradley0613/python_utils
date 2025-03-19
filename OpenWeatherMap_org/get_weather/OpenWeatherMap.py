3e882f3e2f062950fe2bd6026960dd8abde47a07
RBradley0613 <36904517+RBradley0613@users.noreply.github.com>
2025-03-19 16:53:19 -0700

    cleanup
    - added error handling in main.py
    - refactoring in main.py to remove exit() call

--- OpenWeatherMap_org/get_weather/OpenWeatherMap.py
+++ OpenWeatherMap_org/get_weather/OpenWeatherMap.py
@@ -1,6 +1,8 @@
 import requests
 from pathlib import Path
 
+_weather_base_url = "http://api.openweathermap.org/data/2.5/weather?"
+_units = "imperial"
 
 def GetAppId():
     #Requires the file app_id.txt exists.  The only contents of this file are the AppId provided by OpenWeatherMap.com.
@@ -12,32 +14,22 @@ def GetAppId():
 
 def GetWeatherByZip(zip):
     app_id = GetAppId()
-
-    weather_base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    units = "imperial"
-
-    api_call = weather_base_url + "units=" + units + "&zip=" + zip + "&appid=" + app_id
-
+    api_call = _weather_base_url + "units=" + _units + "&zip=" + zip + "&appid=" + app_id
     resp = requests.get(api_call).json()
     return resp
 
 
 def GetWeatherByCityStateCountry(city, state, country):
     app_id = GetAppId()
-
-    weather_base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    units = "imperial"
-
-    api_call = weather_base_url + "units=" + units + "&appid=" + app_id + "&q=" + city 
+    api_call = _weather_base_url + "units=" + _units + "&appid=" + app_id + "&q=" + city 
 
     #Add state if not empty
     if len(state) > 0:
         api_call += f",{state}"
-
+        
     #add country if not empty
     if len(country) > 0:
         api_call += f",{country}"
 
     resp = requests.get(api_call).json()
-
     return resp
--- OpenWeatherMap_org/main.py
+++ OpenWeatherMap_org/main.py
@@ -1,5 +1,20 @@
 from get_weather import OpenWeatherMap
 
+def DisplayResponse(resp):
+    if resp["cod"] == 200:
+        weather = resp["weather"][0]["main"]
+        temp = resp["main"]["temp"]
+        feels_like = resp["main"]["feels_like"]
+
+        print()
+        print(f"Current weather conditions for zip code {zip}")
+        print(f"{weather}")
+        print(f"Temperature: {temp}")
+        print(f"Feels Like: {feels_like}")
+        print()
+    else:
+        print(f"Error: ({resp["cod"]}) {resp["message"]}")
+
 print("Main Menu")
 print("1.  Get weather by zip code")
 print("2.  Get weather by city, state, and country")
@@ -10,23 +25,13 @@ match selection:
         print()
         zip = input("Enter a valid zip code: ")
         resp = OpenWeatherMap.GetWeatherByZip(zip)
+        DisplayResponse(resp)
     case "2":
         print()
         city = input("Enter city name: ")
         state = input("Enter state code: ")
         country = input("Enter country code: ")
         resp = OpenWeatherMap.GetWeatherByCityStateCountry(city, state, country)
+        DisplayResponse(resp)
     case _:
         print("You must enter 1 or 2")
-        exit()
-
-weather = resp["weather"][0]["main"]
-temp = resp["main"]["temp"]
-feels_like = resp["main"]["feels_like"]
-
-print()
-print(f"Current weather conditions for zip code {zip}")
-print(f"{weather}")
-print(f"Temperature: {temp}")
-print(f"Feels Like: {feels_like}")
-print()
